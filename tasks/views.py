7f4ca278563ab06dcc68d083ebc74f921f7171d1
Tamby Mampionona <theriharson@email.com>
2024-03-15 12:49:46 +0300

    Empêcher l'ajout d'une collection en doublon

--- tasks/views.py
+++ tasks/views.py
@@ -13,5 +13,8 @@ def index(request):
 
 def add_collection(request):
     collection_name = escape(request.POST.get("collection-name"))
-    Collection.objects.create(name=collection_name)
+    collection, created = Collection.objects.get_or_create(name=collection_name)
+    if not created:
+        return HttpResponse("La collection existe déjà.")
+
     return redirect('home')
