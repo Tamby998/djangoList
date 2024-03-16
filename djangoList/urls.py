2dd55f732f9a9a888c04c03575feba6c90bd2de8
Tamby Mampionona <theriharson@email.com>
2024-03-16 13:18:07 +0300

    Changer les tâches affichées

--- djangoList/urls.py
+++ djangoList/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', task_views.index, name="home"),
     path('add-collection/', task_views.add_collection, name="add-collection"),
     path('add-task/', task_views.add_task, name="add-task"),
-    path('get-tasks/<int_collection_pk>/', task_views.get_tasks, name="get-tasks"),
+    path('get-tasks/<int:collection_pk>/', task_views.get_tasks, name="get-tasks"),
     path('admin/', admin.site.urls),
 ]
